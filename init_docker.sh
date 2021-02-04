bf740d7c0ab8b4f5982c6d2dc544c2d5d16afeba
Rezon Jovian <rezonjov@gmail.com>
2021-02-04 17:55:27 +0700

    commit: add test

---
+++ init_docker.sh
@@ -0,0 +1,17 @@
+docker run --name jenkins-blueocean --rm --detach \
+  --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
+  --publish 8080:8080 --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  --volume "$HOME":/home \
+  myjenkins-blueocean:1.1
+
+
+docker run --name jenkins-docker --rm --detach \
+  --privileged --network jenkins --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 3000:3000 \
+  --publish 2376:2376 docker:dind
